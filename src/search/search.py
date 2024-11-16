f22b9530e5107536eb0511a883a8b28989d9f5db
opsullivan85 <opsullivan@wpi.edu>
2024-11-16 13:13:04 -0500

    refined interface

--- src/search/search.py
+++ src/search/search.py
@@ -5,13 +5,24 @@ This file contains the main interface for the search module.
 import numpy as np
 from holonomic_quad import HoloQuadState
 from states import Position
+from dataclasses import dataclass
 
-example_search_output = [
-    HoloQuadState(x=0, y=0, theta=0, x_velocity=0, y_velocity=0, omega=0),
-    HoloQuadState(x=1, y=1, theta=-np.pi / 4, x_velocity=0, y_velocity=0, omega=0),
-    HoloQuadState(x=1, y=3, theta=-np.pi / 2, x_velocity=0, y_velocity=0, omega=0),
-    HoloQuadState(x=3, y=3, theta=0, x_velocity=0, y_velocity=0, omega=0),
-]
+
+@dataclass
+class SearchResult:
+    sucess: bool
+    result: list[HoloQuadState] | None
+
+
+example_search_output = SearchResult(
+    True,
+    [
+        HoloQuadState(x=0, y=0, theta=0, x_velocity=0, y_velocity=0, omega=0),
+        HoloQuadState(x=1, y=1, theta=-np.pi / 4, x_velocity=0, y_velocity=0, omega=0),
+        HoloQuadState(x=1, y=3, theta=-np.pi / 2, x_velocity=0, y_velocity=0, omega=0),
+        HoloQuadState(x=3, y=3, theta=0, x_velocity=0, y_velocity=0, omega=0),
+    ],
+)
 """
 Example of an output from the search function. There probably would be 
 non-zero values for x_velocity, y_velocity, and omega, but I expect us to throw
@@ -28,12 +39,16 @@ def search(
     initial_state: Position,
     final_state: Position,
     dt: float,
-) -> list[HoloQuadState]:
+) -> SearchResult:
     """High level interface for searching through the world
+    
+    Performs a search from initial_state to final_state through a map.
+    The robot is defined by the HoloQuad implementation.
 
     Args:
         map (np.ndarray): 2d array of the map with height values (m).
-            map[0,0] placed on [0,0] in space. Directed in +x +y.
+            Map[0,0] placed at (0,0) in space, directed in +x +y.
+            `impassible` cells should have values large positive values (np.inf)
         map_cell_size (float): size of each cell in the map (m).
             cells are assumed to be square.
         initial_state (Position): Initial state in world space.
@@ -41,6 +56,7 @@ def search(
         dt (float): dt.
 
     Returns:
-        list[HoloQuadState]: list of states leading to the solution
+        SearchResult: Boolean indicating sucess, and
+            a list of states leading to the solution (assuming sucess)
     """
     raise NotImplementedError()
